8618b34d612acb51d4d5a8d0f1e74b91650a80a3
shibaanaik <148673205+shibaanaik@users.noreply.github.com>
2025-03-07 10:50:11 +0530

    app.py

--- app.py
+++ app.py
@@ -11,65 +11,65 @@ model = load_model(r'C:\Users\KIIT\Documents\stock price predction\stock predcti
 st.header('Stock Market Predictor')
 
 stock =st.text_input('Enter Stock Symnbol', 'GOOG')
-start = '2012-01-01'
-end = '2022-12-31'
+start = '2014-01-01'
+end = '2025-03-01'
 
-data = yf.download(stock, start ,end)
+df = yf.download(stock, start ,end)
 
 st.subheader('Stock Data')
-st.write(data)
+st.write(df)
 
-data_train = pd.DataFrame(data.Close[0: int(len(data)*0.80)])
-data_test = pd.DataFrame(data.Close[int(len(data)*0.80): len(data)])
+df_train = pd.DataFrame(df.Close[0: int(len(df)*0.80)])
+df_test = pd.DataFrame(df.Close[int(len(df)*0.80): len(df)])
 
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler(feature_range=(0,1))
 
-pas_100_days = data_train.tail(100)
-data_test = pd.concat([pas_100_days, data_test], ignore_index=True)
-data_test_scale = scaler.fit_transform(data_test)
+pas_100_days = df_train.tail(100)
+df_test = pd.concat([pas_100_days, df_test], ignore_index=True)
+df_test_scale = scaler.fit_transform(df_test)
 
 st.subheader('Price vs MA50')
-ma_50_days = data.Close.rolling(50).mean()
+ma_50_days = df.Close.rolling(50).mean()
 fig1 = plt.figure(figsize=(8,6))
 plt.plot(ma_50_days, 'r')
-plt.plot(data.Close, 'g')
+plt.plot(df.Close, 'g')
 plt.show()
 st.pyplot(fig1)
 
 st.subheader('Price vs MA50 vs MA100')
-ma_100_days = data.Close.rolling(100).mean()
+ma_100_days = df.Close.rolling(100).mean()
 fig2 = plt.figure(figsize=(8,6))
 plt.plot(ma_50_days, 'r')
 plt.plot(ma_100_days, 'b')
-plt.plot(data.Close, 'g')
+plt.plot(df.Close, 'g')
 plt.show()
 st.pyplot(fig2)
 
 st.subheader('Price vs MA100 vs MA200')
-ma_200_days = data.Close.rolling(200).mean()
+ma_200_days = df.Close.rolling(200).mean()
 fig3 = plt.figure(figsize=(8,6))
 plt.plot(ma_100_days, 'r')
 plt.plot(ma_200_days, 'b')
-plt.plot(data.Close, 'g')
+plt.plot(df.Close, 'g')
 plt.show()
 st.pyplot(fig3)
 
 x = []
 y = []
 
-for i in range(100, data_test_scale.shape[0]):
-    x.append(data_test_scale[i-100:i])
-    y.append(data_test_scale[i,0])
+for i in range(100, df_test_scale.shape[0]):
+    x.append(df_test_scale[i-100:i])
+    y.append(df_test_scale[i,0])
 
 x,y = np.array(x), np.array(y)
 
 predict = model.predict(x)
 
-scale = 1/scaler.scale_
+s = 1/scaler.scale_
 
-predict = predict * scale
-y = y * scale
+predict = predict * s
+y = y * s
 
 st.subheader('Original Price vs Predicted Price')
 fig4 = plt.figure(figsize=(8,6))
